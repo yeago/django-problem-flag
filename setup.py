b3cc7e17e2647e9896756efb057496cc5a79a138
yeago <yeago@tappedout.net>
2011-03-21 22:38:20 -0500

    reorganized

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-problem-flag',
+    version="0.0",
+    author='Steve Yeago',
+    author_email='subsume@gmail.com',
+    description='Managing problem-flags in Django',
+    url='http://github.com/subsume/django-problem-flag',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
